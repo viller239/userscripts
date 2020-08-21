bf62a4a569e16ae71b76056157d1ee1a5c060b95
Anton Viller <viller239@gmail.com>
2020-08-21 16:11:22 +0100

    Update pfpt-jira-link-in-pr-title.user.js

--- src/pfpt-jira-link-in-pr-title.user.js
+++ src/pfpt-jira-link-in-pr-title.user.js
@@ -1,18 +1,21 @@
 // ==UserScript==
 // @name         Replaces Jira ticket name in PR title with jira link
 // @namespace    https://github.com
-// @version      0.1.6
+// @version      0.1.7
 // @description
 // @author       https://github.com/viller239
 // @match        https://*.github.com/weblifeio/*/pull*
 // @run-at       document-idle
 // ==/UserScript==
-
-Array.from(document.querySelectorAll('.js-issue-title'))
-    .map((el) => ([el, el.innerText.match(/(\[?ISO[-\s]*(\d+)\]?)/i)]))
-    .filter(([, match]) => match && match[2])
-    .forEach(([el, match]) => {
-        const t = el.innerText;
-        el.innerHTML = `${t.substring(0, match.index)} <a href="https://jira.proofpoint.com/jira/browse/ISO-${match[2]}" target=_blank>${match[0]}</a> ${t.substring(match.index + match[0].length)}`;
-    });
+(function addLink() {
+    Array.from(document.querySelectorAll('.js-issue-title'))
+        .filter((el) => !el.querySelector('a[heref~="jira.proofpoint"]'))
+        .map((el) => ([el, el.innerText.match(/(\[?ISO[-\s]*(\d+)\]?)/i)]))
+        .filter(([, match]) => match && match[2])
+        .forEach(([el, match]) => {
+            const t = el.innerText;
+            el.innerHTML = `${t.substring(0, match.index)} <a href="https://jira.proofpoint.com/jira/browse/ISO-${match[2]}" target=_blank>${match[0]}</a> ${t.substring(match.index + match[0].length)}`;
+        });
     
+    setTimeout(addLink, 250);
+})();
