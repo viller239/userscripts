4b6bf00b7a7b20471e8482e935d250bb3830c81e
Anton Viller <viller239@gmail.com>
2020-08-27 23:15:34 +0100

    Update fuck-theme-color.user.js

--- src/fuck-theme-color.user.js
+++ src/fuck-theme-color.user.js
@@ -2,12 +2,14 @@
 // ==UserScript==
 // @name         Fuck theme color
 // @namespace    https://github.com/viller239
-// @version      0.1
+// @version      0.2
 // @description  Deleteing custom theme color from websites
 // @author       https://github.com/viller239
 // @match        *://*/*
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 
-Array.from(document.querySelector('meta[name="theme-color"]'))
-  .forEach((el) => el.parentNode.removeChild(el));
+const el = document.createElement('meta');
+el.setAttribute('name', 'theme-color');
+el.setAttribute('content', 'rgba(0,0,0,0)');
+document.documentElement.appendChild(el);
