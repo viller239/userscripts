0ba2638084921be53aa2c651597a96d4bb7ca72a
Anton Viller <viller239@gmail.com>
2020-10-25 15:53:13 +0000

    Update mobile-youtube-autorepeat.user.js

--- src/mobile-youtube-autorepeat.user.js
+++ src/mobile-youtube-autorepeat.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Loops mobile youtube playback
 // @namespace    https://github.com/viller239
-// @version      0.1.0
+// @version      0.1.1
 // @description  Presses "repeat" button as soon as it becomes visible
 // @updateURL    https://viller239.github.io/userscripts/src/mobile-youtube-autorepeat.user.js
 // @downloadURL  https://viller239.github.io/userscripts/src/mobile-youtube-autorepeat.user.js
 // @author       https://github.com/viller239
-// @match        *://m.youtube.com/watch?*
+// @match        *://m.youtube.com/*
 // @run-at       document-end
 // ==/UserScript==
 
