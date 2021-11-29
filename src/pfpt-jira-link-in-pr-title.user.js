c48185da2a1a6809f8494b7daec114ecbda3329d
Anton Viller <viller239@gmail.com>
2021-11-29 15:19:16 +0000

    Update pfpt-jira-link-in-pr-title.user.js
    version bump

--- src/pfpt-jira-link-in-pr-title.user.js
+++ src/pfpt-jira-link-in-pr-title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replaces Jira ticket name in PR title with jira link
 // @namespace    https://github.com
-// @version      0.1.7
+// @version      0.1.8
 // @description
 // @author       https://github.com/viller239
 // @match        https://*.github.com/weblifeio/*/pull*
